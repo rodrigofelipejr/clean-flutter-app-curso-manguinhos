821fa078947cab771c55be67528d35a55988cf67
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-05-11 21:10:33 -0300

    ensure HttpAdapter throws NotFoundError on 404

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -96,6 +96,12 @@ main() {
       expect(future, throwsA(HttpError.forbidden));
     });
 
+    test('should return NotFoundError if post returns 404', () async {
+      mockResponse(404);
+      final future = sut.request(url: url, method: 'post');
+      expect(future, throwsA(HttpError.notFound));
+    });
+
     test('should return ServerError if post returns 500', () async {
       mockResponse(500);
       final future = sut.request(url: url, method: 'post');
