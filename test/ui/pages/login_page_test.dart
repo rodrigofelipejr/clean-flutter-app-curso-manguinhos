9c75e373ec523033239f07a380f730acb134a599
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-05-13 19:46:56 -0300

    move page initialization to a helper method

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -3,9 +3,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fordev/ui/pages/pages.dart';
 
 main() {
-  testWidgets('should load if correct initial state', (WidgetTester tester) async {
+  Future<void> loadPage(WidgetTester tester) async {
     final loginPage = MaterialApp(home: LoginPage());
     await tester.pumpWidget(loginPage);
+  }
+
+  testWidgets('should load if correct initial state', (WidgetTester tester) async {
+    await loadPage(tester);
 
     final emailTextChildren = find.descendant(of: find.bySemanticsLabel('E-mail'), matching: find.byType(Text));
     expect(
