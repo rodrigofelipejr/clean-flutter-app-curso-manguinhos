53dcf4df78c7c2b1e6d8ab1b0dd6633449f5b28b
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-05-14 22:20:09 -0300

    move StreamLoginPresenter to its own file

---
+++ lib/presentation/presenters/stream_login_presenter.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+import '../dependencies/dependencies.dart';
+
+class LoginState {
+  String? emailError;
+}
+
+class StreamLoginPresenter {
+  final Validation validation;
+  final _controller = StreamController<LoginState>.broadcast();
+  var _state = LoginState();
+
+  Stream<String?> get emailErrorStream => _controller.stream.map((state) => state.emailError);
+  StreamLoginPresenter({required this.validation});
+
+  void validateEmail(String email) {
+    _state.emailError = validation.validate(field: 'email', value: email);
+    _controller.add(_state);
+  }
+}
--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,31 +1,12 @@
-import 'dart:async';
-
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
+import 'package:fordev/presentation/presenters/presenters.dart';
 import 'package:fordev/presentation/dependencies/validation.dart';
 
 import 'stream_login_presenter_test.mocks.dart';
-
-class LoginState {
-  String? emailError;
-}
-
-class StreamLoginPresenter {
-  final Validation validation;
-  final _controller = StreamController<LoginState>.broadcast();
-  var _state = LoginState();
-
-  Stream<String?> get emailErrorStream => _controller.stream.map((state) => state.emailError);
-  StreamLoginPresenter({required this.validation});
-
-  void validateEmail(String email) {
-    _state.emailError = validation.validate(field: 'email', value: email);
-    _controller.add(_state);
-  }
-}
 
 @GenerateMocks([], customMocks: [MockSpec<Validation>(as: #ValidationMock, returnNullOnMissingStub: true)])
 void main() {
