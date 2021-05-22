1f1759a3d5d8a1e6ebbfa61636629e74a1e9cbca
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-05-22 13:10:32 -0300

    move sut creation to the setup method

--- test/data/usecases/save_current_account/local_save_current_account_test.dart
+++ test/data/usecases/save_current_account/local_save_current_account_test.dart
@@ -1,11 +1,11 @@
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
 import 'package:fordev/domain/entities/entities.dart';
 import 'package:fordev/domain/helpers/helpers.dart';
 import 'package:fordev/domain/usecases/usecases.dart';
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
 
 import 'local_save_current_account_test.mocks.dart';
 
@@ -27,24 +27,25 @@ class LocalSaveCurrentAccount implements SaveCurrentAccount {
   }
 }
 
-@GenerateMocks([], customMocks: [
-  MockSpec<SaveSecureCacheStorage>(as: #SaveSecureCacheStorageMock, returnNullOnMissingStub: true),
-])
+@GenerateMocks([],
+    customMocks: [MockSpec<SaveSecureCacheStorage>(as: #SaveSecureCacheStorageMock, returnNullOnMissingStub: true)])
 main() {
-  test('Should call SaveSecureCacheStorage with correct values', () async {
-    final saveSecureCacheStorage = SaveSecureCacheStorageMock();
-    final sut = LocalSaveCurrentAccount(saveSecureCacheStorage: saveSecureCacheStorage);
-    final account = AccountEntity(faker.guid.guid());
+  late LocalSaveCurrentAccount sut;
+  late SaveSecureCacheStorageMock saveSecureCacheStorage;
+  late AccountEntity account;
 
+  setUp(() {
+    saveSecureCacheStorage = SaveSecureCacheStorageMock();
+    sut = LocalSaveCurrentAccount(saveSecureCacheStorage: saveSecureCacheStorage);
+    account = AccountEntity(faker.guid.guid());
+  });
+
+  test('Should call SaveSecureCacheStorage with correct values', () async {
     await sut.save(account);
     verify(saveSecureCacheStorage.saveSecure(key: 'token', value: account.token)).called(1);
   });
 
   test('Should throw UnexpectedError if SaveSecureCacheStorage throws', () async {
-    final saveSecureCacheStorage = SaveSecureCacheStorageMock();
-    final sut = LocalSaveCurrentAccount(saveSecureCacheStorage: saveSecureCacheStorage);
-    final account = AccountEntity(faker.guid.guid());
-
     when(saveSecureCacheStorage.saveSecure(key: anyNamed('key'), value: anyNamed('value'))).thenThrow(Exception());
 
     final future = sut.save(account);
