9c5e3c7400f215035f113f11d31564239ffddaaf
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-05-22 12:06:08 -0300

    organize folders

--- lib/domain/usecases/save_current_account/save_current_account.dart
+++ lib/domain/usecases/save_current_account/save_current_account.dart
@@ -1,5 +1,5 @@
 //ANCHOR - Esse método não sabe se o é seguro ou não, ele apenas salva os dados
-import '../entities/account_entity.dart';
+import '../../entities/account_entity.dart';
 
 abstract class SaveCurrentAccount {
   Future<void> save(AccountEntity account);
--- lib/domain/usecases/usecases.dart
+++ lib/domain/usecases/usecases.dart
@@ -1,3 +1,3 @@
 export 'authentication/authentication.dart';
 export 'authentication/authentication_params.dart';
-export 'save_current_account.dart';
+export 'save_current_account/save_current_account.dart';
