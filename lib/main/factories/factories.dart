403498975941115f2351234cfd8c030d6fe2e2c8
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-06-08 07:48:45 -0300

    refector exports

--- lib/main/factories/factories.dart
+++ lib/main/factories/factories.dart
@@ -1,4 +1,4 @@
-export './http/http.dart';
-export './usecases/usecases.dart';
-export './pages/pages.dart';
-export './cache/cache.dart';
+export 'http/http.dart';
+export 'usecases/usecases.dart';
+export 'pages/pages.dart';
+export 'cache/cache.dart';
