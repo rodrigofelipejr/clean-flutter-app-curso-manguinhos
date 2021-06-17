6de7d2030534d9dac01a4767cbdb05799d367c79
Rodrigo Felipe <rodrigofelipejr@gmail.com>
2021-06-17 14:32:03 -0300

    ensure LocalLoadSurveys delete cache if its incomplete

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -147,12 +147,24 @@ void main() {
       verify(cacheStorage.fetch('surveys')).called(1);
     });
 
-    test('Should delete cache if is invalid', () async {
+    test('Should delete cache if it is invalid', () async {
       mockFetch([
         {
           'id': faker.guid.guid(),
           'question': faker.randomGenerator.string(10),
           'date': 'invalid date',
+          'didAnswer': 'true',
+        }
+      ]);
+
+      await sut.validate();
+      verify(cacheStorage.delete('surveys')).called(1);
+    });
+
+    test('Should delete cache if it is incomplete', () async {
+      mockFetch([
+        {
+          'date': '2021-03-06T00:00:00Z',
           'didAnswer': 'true',
         }
       ]);
